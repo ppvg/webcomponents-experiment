4881050ee0664d4cb346cce7ee448e373a648159
Peter-Paul van Gemerden <mail@ppvg.nl>
2023-11-24 07:17:49 +0100

    add code to prerendered source as html comment

--- src/lib/Highlight.svelte
+++ src/lib/Highlight.svelte
@@ -5,9 +5,13 @@
 
     /** @type {string} */
     export let code = "";
+    $: trimmed = trim(code);
 
     /** @type {'html'|'css'} */
     export let language = "html";
 </script>
 
-<pre><code>{@html hljs.highlight(trim(code), { language }).value}</code></pre>
+{@html `<!--
+${trimmed}
+-->`}
+<pre><code>{@html hljs.highlight(trimmed, { language }).value}</code></pre>
